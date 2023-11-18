381d58b0c3b23378faaee72fb56f9c086d6f83ff
Tomás Lucas <tomaslucas@gmail.com>
2023-11-18 22:50:14 +0100

    ejemplos con título, subtítulos y texto en clt_app

--- clt_app/clt_demo.py
+++ clt_app/clt_demo.py
@@ -2,14 +2,22 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-perc_heads = st.number_input(label = "Opciones de que salga Cara", 
+st.title('Illustrating the Central Limit Theorem with Streamlit')
+st.subheader('An App by Tyler Richards')
+st.write(('This app simulates a thousand coin flips using the chance of heads input below,'
+     'and then samples with replacement from that population and plots the histogram of the'
+     ' means of the samples in order to illustrate the central limit theorem!'))
+
+perc_heads = st.number_input(label = 'Chance of Coins Landing on Heads', 
                              min_value = 0.0, 
                              max_value = 1.0, 
                              value = .5)
+
 binom_dist = np.random.binomial(1, perc_heads, 1000)
 list_of_means = []
 for i in range(0, 1000):
     list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 fig, ax = plt.subplots()
-ax = plt.hist(list_of_means, range=[0,1])
+plt.hist(list_of_means, range=[0,1])
+
 st.pyplot(fig)
