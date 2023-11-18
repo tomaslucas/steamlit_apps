732cbd7aef5251d4302e0e9bd5c3b35118402242
Tomás Lucas <tomaslucas@gmail.com>
2023-11-18 22:38:27 +0100

    incluyo entrada de valores numéricos en clt_app

--- clt_app/clt_demo.py
+++ clt_app/clt_demo.py
@@ -2,10 +2,14 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-binom_dist = np.random.binomial(1, .5, 1000)
+perc_heads = st.number_input(label = "Opciones de que salga Cara", 
+                             min_value = 0.0, 
+                             max_value = 1.0, 
+                             value = .5)
+binom_dist = np.random.binomial(1, perc_heads, 1000)
 list_of_means = []
 for i in range(0, 1000):
     list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 fig, ax = plt.subplots()
-ax = plt.hist(list_of_means)
+ax = plt.hist(list_of_means, range=[0,1])
 st.pyplot(fig)
