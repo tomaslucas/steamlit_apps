acfa5170bad015035262bb41fd8c6c2b6f009e85
Tomás Lucas <tomaslucas@gmail.com>
2023-12-04 11:03:34 +0100

    Add cache_data

--- penguin_app/penguins.py
+++ penguin_app/penguins.py
@@ -6,11 +6,33 @@ import seaborn as sns
 st.title("Palmer's Penguins")
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 
+penguin_file = st.file_uploader("Select Your Local Penguins CSV (default provided)")
+
+@st.cache_data()
+def load_file (penguin_file):
+    if penguin_file is not None:
+        penguins_df = pd.read_csv(penguin_file)
+    else:
+        df = pd.read_csv('penguins.csv')
+        # st.stop()
+    return(df)
+
+penguins_df = load_file(penguin_file)
+
 selected_x_var = st.selectbox('What do you want the x variable to be?',
      ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
 selected_y_var = st.selectbox('What about the y?',
      ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
-penguins_df = pd.read_csv('penguins.csv')
+selected_gender = st.selectbox('What gender do you want to filter for?',
+                               ['all penguins', 'male penguins', 'female penguins'])
+
+if selected_gender == 'male penguins':
+    penguins_df = penguins_df[penguins_df['sex'] == 'male']
+elif selected_gender == 'female penguins':
+    penguins_df = penguins_df[penguins_df['sex'] == 'female']
+else:
+    pass
+
 
 alt_chart = (
     alt.Chart(penguins_df, title=f"Scatterplot of Palmer's Penguins")
