fe81b17f3a8acd9deee9f15d7a56ace526718a2f
Tomás Lucas <tomaslucas@gmail.com>
2023-12-23 19:23:25 +0100

    Adapto el código a Polars para genere la misma salida y accurary que con Pandas

---
+++ penguin_ml/penguins_pl_ml.py
@@ -0,0 +1,50 @@
+import polars as pl
+import polars.selectors as cs
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+penguin_df = pl.read_csv('penguins.csv').drop_nulls()
+
+expr = (
+    penguin_df
+    .select(pl.col('species'))
+    .with_columns(rank = pl.col('species').rank('dense')-1)
+)
+
+uniques = expr.unique().sort('rank').select('species').to_numpy()
+output = expr.select('rank').to_series().to_numpy()
+
+features = (penguin_df
+            .select(pl.all().exclude(['species', 'year']))
+            .to_dummies(cs.string())
+            .select(
+                ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g',
+                 'island_Biscoe', 'island_Dream', 'island_Torgersen', 'sex_female',
+                 'sex_male']
+            )
+           )
+
+x_train, x_test, y_train, y_test = train_test_split(
+    features.to_numpy(), output, test_size=.8, random_state=15)
+rfc = RandomForestClassifier(random_state=15)
+rfc.fit(x_train, y_train)
+y_pred = rfc.predict(x_test)
+score = accuracy_score(y_pred, y_test)
+print(f'Our accuracy score for this model is {score}')
+
+# Verificar predicciones adicionales
+# print(f'Return {rfc.predict([[50,10,5,5,1,0,0,1,0]])} : {uniques[rfc.predict([[50,10,5,5,1,0,0,1,0]])]}')
+# print(f'Return {rfc.predict([[10,30,15,5,0,0,0,0,1]])} : {uniques[rfc.predict([[10,30,15,5,0,0,0,0,1]])]}')
+# print(f'Return {rfc.predict([[46,13,211,5500,0,0,1,0,1]])} : {uniques[rfc.predict([[46,13,211,5500,0,0,1,0,1]])]}')
+# print(f'Return {rfc.predict([[0,0,0,0,1,0,0,1,0]])} : {uniques[rfc.predict([[0,0,0,0,1,0,0,1,0]])]}')
+# print(uniques.tolist())
+      
+rf_pickle = open('random_forest_penguin.pickle', 'wb')
+pickle.dump(rfc, rf_pickle)
+rf_pickle.close()
+
+output_pickle = open('output_penguin.pickle', 'wb')
+pickle.dump(uniques, output_pickle)
+output_pickle.close()
